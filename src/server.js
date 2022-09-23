776fe51611135711f6714a2ceb21764254be8fd8
ale <ale@ale.com>
2022-09-22 21:22:30 -0300

    feat: connect with database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = require('./connect');
+pool.connect();
 
 const port = 3000;
 
@@ -8,6 +14,18 @@ app.use(express.json());
 
 app.get('/', (_, response) => {
     return response.status(201).json({ message: 'Initial API', stack: 'Node JS', database: 'PostgreSQL' })
+});
+
+app.get('/users', async (request, response) => {
+    try {
+        const users = await pool.query('SELECT * FROM users');
+
+        return response.status(201).json(users.rows)
+
+    } catch (error) {
+        return response.status(400).json({ message: `Error: ${error}` })
+    }
 })
+
 
 app.listen(port, () => console.log(`Rodando servidor na porta ${port}`))
